d4cf9ffd95e6da531a07429f77507d86aed0dc82
Vadim Taratuta <hawaiianwork@mail.ru>
2024-11-26 02:54:24 +0300

    feat: other updates in schema, migration, etc.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,29 @@ import { ArtistModule } from './modules/artist/artist.module';
 import { AlbumModule } from './modules/album/album.module';
 import { FavoritesModule } from './modules/favorites/favorites.module';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './common/logger/logger.interceptor';
+import { LoggerService } from './common/logger/logger.service';
+import { ExceptionFilter } from './common/filter/exception-filter.interceptor';
 
 @Module({
+  providers: [
+    LoggerService,
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
     UserModule,
     TrackModule,
     ArtistModule,
